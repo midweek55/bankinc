a052a45aab2a1afcb7da3a6b3fde05a5d9e6aad0
midweek <angelomiguelpinedamendoza@gmail.com>
2025-03-03 06:44:11 -0500

    Corregir problema de lazy loading en la relación entre entidades

--- src/main/java/com/bankinc/cards/model/Transaccion.java
+++ src/main/java/com/bankinc/cards/model/Transaccion.java
@@ -3,6 +3,7 @@ package com.bankinc.cards.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
 import lombok.Data;
 import java.time.LocalDateTime;
 
@@ -12,7 +13,7 @@ public class Transaccion {
     @Id
     private String id;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     private Tarjeta tarjeta;
     
     private double monto;
